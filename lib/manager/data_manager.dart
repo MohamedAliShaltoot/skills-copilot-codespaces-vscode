61e23486fb8cc6ff55fcbcc0cdda8f4487b6b628
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-08-07 01:09:12 +0300

    add manager to get data from api , add to cart , remove from cart

---
+++ lib/manager/data_manager.dart
@@ -0,0 +1,67 @@
+
+import 'dart:convert';
+
+import 'package:copilot_learning/models/data_models.dart';
+
+import 'package:http/http.dart' as http;
+
+
+class DataManager {
+  List<Category>? _menu;
+  List<ItemInCart> cart = [];  // quantity , product object
+
+  cartAdd(Product p) {
+    bool found = false;
+    for (var item in cart) {
+      if (item.product.id == p.id) {
+        item.quantity++;
+        found = true;
+      }
+    }
+    if (!found) {
+      cart.add(ItemInCart(product: p, quantity: 1));
+    }
+  }
+
+  cartDelete(Product p) {
+    cart.removeWhere((element) => element.product.id == p.id);
+  }
+
+  cartClear() {
+    cart.clear();
+  }
+
+  double cartTotal() {
+    var sum = 0.0;
+    for (var item in cart) {
+      sum += item.quantity * item.product.price;
+    }
+    return sum;
+  }
+
+  fetchMenu() async {
+    try {
+      const url = 'https://firtman.github.io/coffeemasters/api/menu.json';
+      var response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        _menu = [];
+        var decodedData = jsonDecode(response.body) as List<dynamic>;
+        for (var json in decodedData) {
+          _menu?.add(Category.fromJson(json));
+        }
+      } else {
+        throw Exception("Error loading data");
+      }
+    } catch (e) {
+      throw Exception("Error loading data");
+    }
+  }
+
+  Future<List<Category>> getMenu() async {
+    if (_menu == null) {
+      await fetchMenu();
+    }
+    return _menu!;
+  }
+}
